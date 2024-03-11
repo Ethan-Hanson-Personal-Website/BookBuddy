3b9607e08e42d74985583a7761af18a817ac4015
Alivar <ethanhanson9@gmail.com>
2024-03-10 19:35:07 -0700

    tried to fix

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,15 +1,14 @@
 //TODO make a navbar that has links to the following pages: Home, Books, Login, Register, Account, with books as the home page
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Navigations() {
+export default function Navigations({user}) {
     return (
         <nav>
             <Link to='/'>Home</Link>
             <Link to='/books'>Books</Link>
             <Link to='/login'>Login</Link>
             <Link to='/register'>Register</Link>
-            <Link to='/account'>Account</Link>
+            {user && <Link to='/account'>Account</Link>}
         </nav>
     );
 }
