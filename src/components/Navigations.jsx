dd1d22e6936b38a8797d141f36a6c96a4ceeec58
Alivar <ethanhanson9@gmail.com>
2024-03-11 15:52:27 -0700

    account should appear on login now

--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -1,14 +1,20 @@
 //TODO make a navbar that has links to the following pages: Home, Books, Login, Register, Account, with books as the home page
 import { Link } from 'react-router-dom';
 
-export default function Navigations({user}) {
+export default function Navigations() {
+    const isLoggedIn = !!localStorage.getItem('token');
+
     return (
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/books'>Books</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
-            {user && <Link to='/account'>Account</Link>}
+            <Link className="nav-link" to='/books'>Books</Link>
+            {isLoggedIn ? (
+                <Link className="nav-link" to='/account'>Account</Link>
+            ) : (
+                <>
+                    <Link className="nav-link" to='/login'>Login</Link>
+                    <Link className="nav-link" to='/register'>Register</Link>
+                </>
+            )}
         </nav>
     );
 }
